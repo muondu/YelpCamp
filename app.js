065aca928aedb7d821246d6299b027d48cdbfa46
muondu <munenemuondu@gmail.com>
2023-02-02 19:40:55 +0300

    Have created mongo db for campgrounds. Have error when running

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
+// Importing express, path and mongoose
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose  = require("mongoose");
+const Campground = require('./models/campground');
+// Connecting Mongoose DB to app.js
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser:true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+// Checks for database connection Error.
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+// Calling express
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {
+// Takes the page to home.ejs
+app.get('/', async (req, res) => {
+    const camp = new Campground({title:'Frontyard', description:'Cheap'});
+    await camp.save();
+    res.send(camp)
+})
+
+app.get('/makecampground', (req, res) => {
     res.render('home')
 })
 
+// Tells express port to go to
 app.listen(3000, ()=> {
     console.log('Serving on pot 3000')
 });
