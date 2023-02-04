c00186a3e630b783d48a6d57f4133393192d7565
muondu <munenemuondu@gmail.com>
2023-02-04 19:47:11 +0300

    Have made the app thus you can edit the campgrounds you have made

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose  = require("mongoose");
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const { render } = require('ejs');
 // Connecting Mongoose DB to app.js
@@ -24,7 +25,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('__method'));
 
 // Takes the page to home.ejs
 app.get('/', async (req, res) => {
@@ -38,7 +40,7 @@ app.get('/makecampground', async (req, res) => {
     res.render('campgrounds/index', {campgrounds})
 });
 
-
+// Takes you to creating new campground
 app.get('/campground/new',  (req, res) = {
     res.render('campgrounds/new')
 });
@@ -48,14 +50,28 @@ app.get('/campgrounds/:id', async(req, res) => {
     res.render('campgrounds/show',{campground});
 });
 
+
+
 app.post('/campgrounds', async(req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/camp/${campground._id}`)
 })
 
+// Takes you to edit campground
+app.get('/campground/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('camgrounds/edit', {campground});
+})
 
 // Tells express port to go to
 app.listen(3000, ()=> {
     console.log('Serving on pot 3000')
+})
+
+app.put('campground/:id', async(req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.camground})
+    res.redirect(`/campgrounds/${campground._id}`)
+
 })
