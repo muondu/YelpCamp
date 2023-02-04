3d108563207b95eed6351ca2c5474b9ee6784ee5
muondu <munenemuondu@gmail.com>
2023-02-04 17:59:50 +0300

    Have created new.ejs that adds campgrounds and you can go back to all campgrounds in new and show. In index.ejs is where you add a new campground

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const mongoose  = require("mongoose");
 const Campground = require('./models/campground');
+const { render } = require('ejs');
 // Connecting Mongoose DB to app.js
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser:true,
@@ -23,6 +24,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}))
+
 // Takes the page to home.ejs
 app.get('/', async (req, res) => {
     const camp = new Campground({title:'Frontyard', description:'Cheap'});
@@ -33,15 +36,26 @@ app.get('/', async (req, res) => {
 app.get('/makecampground', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds})
-})
+});
 
+
+app.get('/campground/new',  (req, res) = {
+    res.render('campgrounds/new')
+});
 
 app.get('/campgrounds/:id', async(req, res) => {
     const campground =  await Campground.findById(req.params.id)
     res.render('campgrounds/show',{campground});
+});
+
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/camp/${campground._id}`)
 })
+
 
 // Tells express port to go to
 app.listen(3000, ()=> {
     console.log('Serving on pot 3000')
-});
+})
