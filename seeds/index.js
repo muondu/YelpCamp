77d9590cf471067e751826fd56a289db05816610
muondu <munenemuondu@gmail.com>
2023-02-08 22:27:29 +0300

    Have added images which are random to the campsite

--- seeds/index.js
+++ seeds/index.js
@@ -28,11 +28,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             //YOUR USER ID
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-           
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://images.unsplash.com/photo-1473773508845-188df298d2d1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODQzNTF8fHx8fHx8MTY3NTg4NDA1OA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut cumque voluptatibus et provident esse id quis vel? Natus asperiores in voluptate, quidem eveniet quos temporibus laborum magnam sequi modi animi!',
+            price 
             })
             
             
