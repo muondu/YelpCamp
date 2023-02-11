cb58930f2b5956853cf077a06a25f7a0d8e1ccf0
muondu <munenemuondu@gmail.com>
2023-02-11 10:45:50 +0300

    Have added JOI Schema Validation and also dowloaded joi to help with the error

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const mongoose  = require("mongoose");
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -47,7 +48,7 @@ app.get('/makecampground',catchAsync( async (req, res) => {
 
 // Takes you to creating new campground
 app.get('/campground/new', (req, res) => {
-    res.render('campgrounds/new')
+    res.render('campgrounds/new')   
 });
 
 app.get('/campgrounds/:id', catchAsync( async(req, res) => {
@@ -58,7 +59,21 @@ app.get('/campgrounds/:id', catchAsync( async(req, res) => {
 
 
 app.post('/campgrounds', catchAsync( async(req, res, next) => {
-    if(!req.body.camground) throw new ExpressError('Invalid Campground Data', 400)
+    // if(!req.body.camground) throw new ExpressError('Invalid Campground Data', 400)
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required(),
+        }).required()
+    })
+    const {error} = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/camp/${campground._id}`)
