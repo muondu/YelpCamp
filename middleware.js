e24673a12ccad2d26f07fb6edfdc807724ed3f46
muondu <munenemuondu@gmail.com>
2023-02-15 18:52:05 +0300

    Done with the topic :D

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
+        req.session.returnTo =  req.originalUrl
         req.flash('error', 'You must be signed in first!');
         // We have put return so this code runs before the code after
         return res.redirect('/login');
