7b8e3fad35249e2637ed906d1d0839d371b5116f
muondu <munenemuondu@gmail.com>
2023-02-15 16:28:46 +0300

    Have fixed errors which may ocurr using Flash

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -32,19 +32,26 @@ router.post('/', validateCampground, catchAsync( async(req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     req.flash('success', 'Successfully created a new campground!')
-
     res.redirect(`/campgrounds/camp/${campground._id}`)
 
 }))
 
 router.get('/:id', catchAsync( async(req, res) => {
     const campground =  await Campground.findById(req.params.id).populate('reviews');
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show',{campground});
 }));
 
 // Takes you to edit campground
 router.get('/:id/edit', catchAsync( async (req, res) => {
     const campground = await Campground.findById(req.params.id)
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground');
+        return res.redirect('/campgrounds');
+    }
     res.render('camgrounds/edit', {campground});
 }))
 
