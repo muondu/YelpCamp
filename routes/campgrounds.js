cabe5f0dd8fd818b9ef83fe29a3b680108c8ca5e
muondu <munenemuondu@gmail.com>
2023-02-15 20:51:34 +0300

    Shows who created the campground as an author

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -34,8 +34,8 @@ router.get('/new', isLoggedIn, (req, res) => {
 });
 
 router.post('/', validateCampground, catchAsync( async(req, res, next) => {
-    // if(!req.body.camground) throw new ExpressError('Invalid Campground Data', 400)
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id
     await campground.save();
     req.flash('success', 'Successfully created a new campground!')
     res.redirect(`/campgrounds/camp/${campground._id}`)
@@ -43,7 +43,8 @@ router.post('/', validateCampground, catchAsync( async(req, res, next) => {
 }))
 
 router.get('/:id',catchAsync( async(req, res) => {
-    const campground =  await Campground.findById(req.params.id).populate('reviews');
+    const campground =  await Campground.findById(req.params.id).populate('reviews').populate('reviews').populate('author');
+    console.log(campground)
     if (!campground) {
         req.flash('error', 'Cannot find that campground');
         return res.redirect('/campgrounds');
--- seeds/index.js
+++ seeds/index.js
@@ -31,6 +31,7 @@ const seedDB = async () => {
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             //YOUR USER ID
+            author: '5f5c330c2cd79d538f2c66d9',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://images.unsplash.com/photo-1473773508845-188df298d2d1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODQzNTF8fHx8fHx8MTY3NTg4NDA1OA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
