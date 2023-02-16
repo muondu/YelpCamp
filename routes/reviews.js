f8f6632f2aaec0bf0a49607c3cd4802b3837bf59
muondu <munenemuondu@gmail.com>
2023-02-16 20:35:15 +0300

    Have made users.js in routes cleaner because all logic for routes are in users.js in controllers

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,6 +4,7 @@ const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 const catchAsync = require('../utils/catchAsync');
 const Review = require('../models/review');
 const Campground = require('../models/campground');
+const reviews = require('../controllers/reviews');
 const ExpressError = require('../utils/ExpressError');
 const { reviewSchema } = require('../schema.js');
 
@@ -14,25 +15,9 @@ const { reviewSchema } = require('../schema.js');
 
 
 
-app.post('/', isLoggedIn ,validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review)
-    review.author = req.user.__id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Created new review!');
-    res.redirect(`/campgrounds/${campground.__id}`);
+app.post('/', isLoggedIn ,validateReview, catchAsync(reviews.createReview));
 
-}))
-
-app.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    const {id, reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Successfully deleted review!');
-    res.redirect(`/campgrounds/${id}`);
-}))
+app.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 
 module.exports = router;   
