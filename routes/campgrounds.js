09045013078fef4235c0ba5d1d48ca765aad676d
muondu <munenemuondu@gmail.com>
2023-02-17 19:42:38 +0300

    Dowloaded Multer to help with uploading images

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const camgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/'})
 
 const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(camgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(camgrounds.createCampground));
+    // .post(isLoggedIn, validateCampground, catchAsync(camgrounds.createCampground));
+    .post(upload.single('image'),(req, res) => {
+        console.log(req.body, req.files);
+        res.send("It worked")
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
