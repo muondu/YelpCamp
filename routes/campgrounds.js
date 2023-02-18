e5e3d2a61e8027ce824046a9a10afe2dcc5b7721
muondu <munenemuondu@gmail.com>
2023-02-18 10:16:38 +0300

    Now you can add images in editing

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,7 +11,7 @@ const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(camgrounds.index))
-    .post(isLoggedIn, upload.single('image'), validateCampground, catchAsync(camgrounds.createCampground));
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(camgrounds.createCampground));
    
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
@@ -20,7 +20,7 @@ router.route('/:id')
     // Shows the campground
     .get(catchAsync(camgrounds.showCampground))
     // Used to update Campground
-    .put(isLoggedIn ,isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .put(isLoggedIn ,isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
     // Used to delete campground
     .delete(isAuthor ,isLoggedIn,catchAsync(camgrounds.deleteCampground))
 
